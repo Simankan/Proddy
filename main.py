5bd227cbd077c43275ae388c81061d9864c98090
Simankan <83919636+Simankan@users.noreply.github.com>
2021-10-29 12:03:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import discord
+from discord.ext import commands
 import os
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '$')
 
 help_text = "Hello! My name is Proddy I have 3 core features:\n```1. Reminder \n2. Polling/voting\n3. Introduction message```"
+welcome_text = ' to Team 6 DL5!\nWe are so excited to have you with us and look forward to working with you!\nYour server role will be assigned automatically. Please let anyone know if you have any questions.'
 #
 #
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
+# @client.event
+# async def on_member_join(member):
+#   role = discord.utils.get(member.server.roles, id="Leader")
+#   await member.add_roles(member, role)
 
-@client.event 
-async def on_message(message):
-  if(message.author == client.user):
+@client.command() 
+async def about(ctx):
+  if(ctx.author == client.user):
     return
+  
+  await ctx.channel.send(help_text.format(client))
 
-  if(message.content.startswith('$hello')):
-    await message.channel.send("Hello, ".format(client) + message.author.mention + "!")
+@client.command()
+async def message(ctx, user:discord.User, *, message=None):
+  msg = 'Welcome, ' + ctx.author.mention + welcome_text
+  await ctx.send('Successful.')
+  await user.send(f'{msg}')
 
-  if(message.content.startswith('$help')):
-    await message.channel.send(help_text.format(client))
+@client.command()
+async def message_custom(ctx, user:discord.User, *, message):
+  await ctx.send('Successful.')
+  await user.send(f'{message}')
+
 
 my_secret = os.environ['BotToken']
 client.run(my_secret)
